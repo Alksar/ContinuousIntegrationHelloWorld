0afb12ada0eb63d18f54c3d45a2518b2e37928de
Сараев Александр Александрович <Alksar@yandex.ru>
2017-08-19 22:25:32 +0300

    Initial commit

---
+++ ContinuousIntegrationHelloWorld.Tests/ValuesControllerTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ContinuousIntegrationHelloWorld.Tests
+{
+    public class ValuesControllerTests
+    {
+        private readonly TestServer _testServer;
+        private readonly HttpClient _httpClient;
+
+        public ValuesControllerTests()
+        {
+            _testServer = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetMethodTest()
+        {
+            var response = await _httpClient.GetAsync("api/values");
+            response.EnsureSuccessStatusCode();
+            string actual = await response.Content.ReadAsStringAsync();
+
+            Assert.Equal("Hello World", actual);
+        }
+    }
+}
